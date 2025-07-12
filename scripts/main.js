69697c61b9e8abedc6f9e2c56417e2ae00547861
Lu <97429118+white-eastern@users.noreply.github.com>
2025-07-12 09:55:07 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -14,7 +14,7 @@ let myHeading = document.querySelector("h1");
 
 function setUserName() {
   const myName = prompt("Please enter your name.");
-  if (!myName) {
+  if (!myName || myName === null) {
     setUserName();
   } else {
     localStorage.setItem("name", myName);
